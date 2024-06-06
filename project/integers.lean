d1de8a72db0855bf95bcc0a64a82d43d869697a2
jyallop <jyallop21@gmail.com>
2024-06-06 17:46:46 -0600

    Remove temp file and add division scaffold

--- project/integers.lean
+++ project/integers.lean
@@ -36,10 +36,16 @@ def pow : Natural → Natural → Natural
 instance : HPow Natural Natural Natural where
   hPow := pow
 
-lemma Natural.add_zero (x : Natural) : x + 0 = x := sorry
+def div : Natural → Natural → Natural
+| x, 1 => x
+| x, n => x
+
+instance : HDiv Natural Natural Natural where
+  hDiv := div
+--lemma Natural.add_zero (x : Natural) : x + 0 = x := sorry
 
 def series : Natural → Natural
 | zero => zero
 | n@(succ m) => (n ^ (2 : Natural)) + (series m)
 
-theorem limit_one : (n : ℕ) → (series n) = n * (n + 1) * (2 * n + 1) / 6 := sorry
+theorem limit_one : (n : Natural) → (series n) = n * (n + 1) * (2 * n + 1) / 6 := sorry
