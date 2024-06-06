ca0889d72712832261406ab34a8958386ab43813
jyallop <jyallop21@gmail.com>
2024-06-06 11:11:27 -0600

    Adds initial Integer/natural code

--- project/integers.lean
+++ project/integers.lean
@@ -1,14 +1,45 @@
 
-inductive Integer where
-| zero : Integer
-| succ : Integer → Integer
+inductive Natural where
+| zero : Natural
+| succ : Natural → Natural
 
-def add : Integer → Integer → Integer
-| Integer.zero, y => y
-| (Integer.succ n), y => add n (Integer.succ y)
+open Natural
+instance : OfNat Natural 0 where
+  ofNat := zero
 
-structure Rational where
-  num : Int
-  den : Nat
-  inv : den ≠ 0
+instance : OfNat Natural (n + 1) where
+  ofNat :=
+    let rec natPlusOne : Nat → Natural
+      | 0 => zero
+      | k + 1 => succ (natPlusOne k)
+    natPlusOne n
 
+def add : Natural → Natural → Natural
+| Natural.zero, y => y
+| (Natural.succ n), y => add n (Natural.succ y)
+
+instance : Add Natural where
+  add := add
+
+def mul : Natural → Natural → Natural
+| zero, _ => zero
+| succ zero, m => m
+| succ n, m => m + (mul n m)
+
+instance : Mul Natural where
+  mul := mul
+
+def pow : Natural → Natural → Natural
+| _, zero => 1
+| x, succ n => x * (pow x n)
+
+instance : HPow Natural Natural Natural where
+  hPow := pow
+
+lemma Natural.add_zero (x : Natural) : x + 0 = x := sorry
+
+def series : Natural → Natural
+| zero => zero
+| n@(succ m) => (n ^ (2 : Natural)) + (series m)
+
+theorem limit_one : (n : ℕ) → (series n) = n * (n + 1) * (2 * n + 1) / 6 := sorry
